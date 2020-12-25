b20d4477a613a40343b93d4adf94c7e405fae228
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-12-25 21:29:48 +0530

    :zap: Validating Inputs and generate JWT Token

--- server/util/validateInput.js
+++ server/util/validateInput.js
@@ -1,43 +1,52 @@
-module.exports.validateRegisterInput = ( username, email, password, confirmPassword) => {
-    const errors = {}
+module.exports.validateRegisterInput = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
 
-    if(username.trim() === ''){
-        errors.username = 'Username Invalid'
+  if (username.trim() === "") {
+    errors.username = "Username Invalid";
+  }
+
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty";
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email is Invalid";
     }
+  }
+  if (password.trim() === "") {
+    errors.password = "Password cannot be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Password does not match";
+  }
 
-    if(email.trim() === ''){
-        errors.email = 'Email must not be empty'
-    }else {
-        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/; 
-        if(!email.match(regEx)){
-            errors.email = 'Email is Invalid'
-        }
-    }
-     if (password.trim() === ''){
-         errors.password = 'Password cannot be empty'
-     }
-
-     else if (password !== confirmPassword){
-         errors.confirmPassword = 'Password does not match';
-     }
-
-     return errors;
-}
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
 
 module.exports.validateLoginInput = (email, password) => {
-    const errors = {}
+  const errors = {};
 
-    if(email.trim() === ''){
-        errors.email = 'Email must not be empty'
-    }else {
-        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/; 
-        if(!email.match(regEx)){
-            errors.email = 'Email is Invalid'
-        }
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty";
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email is Invalid";
     }
-     if (password.trim() === ''){
-         errors.password = 'Password cannot be empty'
-     }
+  }
+  if (password.trim() === "") {
+    errors.password = "Password cannot be empty";
+  }
 
-     return errors;
-}
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
