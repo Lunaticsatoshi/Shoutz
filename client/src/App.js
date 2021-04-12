f00462dec90a1c4edffb5b11f36a7ffd1d5873ff
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-12 17:53:09 +0530

    :zap: Designing Feed Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import Feed from './components/Feed/Feed';
 
 function App() {
   return (
     <div className="app">
+
+      {/* Sidebar Component */}
       <Sidebar />
+      {/* Feed Component */}
+      <Feed/>
     </div>
   );
 }
--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Feed.css";
 
 function Feed() {
-    return (
-        <div>
-            <h1>Feed Page</h1>
-        </div>
-    )
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
